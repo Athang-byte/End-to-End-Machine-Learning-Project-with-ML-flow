f7b43876aaf7e1426fd43106b7ea4528150d136d
Athang-byte <athangsagar10@gmail.com>
2026-04-09 14:02:14 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import MLproject.config.configuration as cfg
+print("Using config file from:", cfg.__file__)
+
 from MLproject import logger
 from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
@@ -13,3 +16,25 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+from MLproject.components.data_validation import DataValidation
+from MLproject.config.configuration import ConfigurationManager
+from MLproject import logger
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
+    config = ConfigurationManager()
+    data_validation_config = config.get_data_validation_config()
+
+    data_validation = DataValidation(config=data_validation_config)
+    data_validation.validate_all_columns()
+
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/MLproject/config/configuration.py
+++ src/MLproject/config/configuration.py
@@ -1,21 +1,20 @@
 from MLproject.constants import *
-from MLproject.utils.common import read_yaml,create_directories
-from MLproject.entity.config_entity import DataIngestionConfig
+from MLproject.utils.common import read_yaml, create_directories
+from MLproject.entity.config_entity import DataIngestionConfig, DataValidationConfig  
 
 
 class ConfigurationManager:
     def __init__(
         self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH,
-        schema_filepath = SCHEMA_FILE_PATH):
+        config_filepath=CONFIG_FILE_PATH,
+        params_filepath=PARAMS_FILE_PATH,
+        schema_filepath=SCHEMA_FILE_PATH):
 
-        self.config = read_yaml (config_filepath)
-        self.params = read_yaml (params_filepath)
-        self.schema = read_yaml (schema_filepath)
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
 
-        create_directories([self.config.artifacts_root]) 
-
+        create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -30,3 +29,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
