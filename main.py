e2a5de210209044b7ae5260030c337a17789facc
Athang-byte <athangsagar10@gmail.com>
2026-04-10 15:20:09 +0530

    model trainer added

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ print("Using config file from:", cfg.__file__)
 
 from MLproject import logger
 from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Injestion Stage"
@@ -22,6 +25,8 @@ from MLproject.components.data_validation import DataValidation
 from MLproject.config.configuration import ConfigurationManager
 from MLproject import logger
 
+
+
 STAGE_NAME = "Data Validation Stage"
 
 try:
@@ -35,6 +40,32 @@ try:
 
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME= "Data Transformation Stage"
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion=DataTransformationTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Training Stage "
+
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj=ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/MLproject/pipeline/stage_02_data_validation.py
+++ src/MLproject/pipeline/stage_02_data_validation.py
@@ -1,18 +1,18 @@
 
-from mlProject.config.configuration import ConfigurationManager 
-from mlProject.components.data_validation import DataValiadtion 
-from mlProject import logger
+from MLproject.config.configuration import ConfigurationManager 
+from MLproject.components.data_validation import DataValidation
+from MLproject import logger
 
 
 STAGE_NAME = "Data Validation stage"
 class DataValidationTrainingPipeline: 
     def __init__(self):
-    pass
+        pass
 
     def main(self):
         config = ConfigurationManager()
         data_validation_config=config.get_data_validation_config()
-        data_validation=DataValiadtion(config-data_validation_config)
+        data_validation=DataValidation(config-data_validation_config)
         data_validation.validate_all_columns()
 
 
